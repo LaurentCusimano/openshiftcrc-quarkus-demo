39665f04cf90752bfdbd438e9e445ea7775d3f85
Laurent Cusimano <laurent.cusimano@gmail.com>
2023-04-13 11:49:51 +0200

    init quarkus project

---
+++ src/main/java/org/acme/rest/json/InfinispanRecord.java
@@ -0,0 +1,14 @@
+package org.acme.rest.json;
+
+public class InfinispanRecord {
+    public String name;
+    public String content;
+
+    public InfinispanRecord() {
+    }
+
+    public InfinispanRecord(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+}
